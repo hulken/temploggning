dc4b61519febafb9a58ed28f681a94fca7237814
sweco-semhul <martin.hultman@sweco.se>
2014-05-20 22:23:25 +0200

    Added weather forecast to web service api

--- web/data/index.php
+++ web/data/index.php
@@ -3,6 +3,7 @@ include_once 'Slim/Slim.php';
 include_once 'settings.php';
 include_once 'Readings.php';
 include_once 'Sensors.php';
+include_once 'WeatherForecast.php';
 
 // Initiate Slim framework
 \Slim\Slim::registerAutoloader();
@@ -31,6 +32,24 @@ $api->put('/sensor/:id', function ($id) use ($api) {
 	$json = json_decode($request->getBody());
 	if(isset($json->name)) {
     	echo($sensors->update($id, $json->name, $json->id, $json->color));
+    }
+});
+// -------------------
+
+// WeatherForecast
+// -------------------
+$api->get('/weatherforecast/:source', function ($source) {
+    $weatherForecast = new WeatherForecast();
+    switch ($source) {
+        case 'all':
+            echo($weatherForecast->all($_GET['lat'],$_GET['lng'],$_GET['place']));
+            break;
+        case 'smhi':
+            echo($weatherForecast->smhi($_GET['lat'],$_GET['lng']));
+            break;
+        case 'yr':
+            echo($weatherForecast->yr($_GET['place']));
+            break;
     }
 });
 // -------------------
