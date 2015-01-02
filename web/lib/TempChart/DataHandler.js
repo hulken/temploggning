07e0c0e1eda51803cbe3f863798664341eb6da07
semhul <martin.hultman@sweco.se>
2015-01-02 13:32:19 +0100

    Fixing loading problems with statistics view. Fixing spacing.

--- web/lib/TempChart/DataHandler.js
+++ web/lib/TempChart/DataHandler.js
@@ -62,7 +62,10 @@ TempChart.DataHandler.prototype = {
     
     $(document).bind('TempChart_load_customReadings', function(e, readings) { 
       console.log('EVENT READ LOAD_CUSTOMREADINGS ::');
-      me.doLoadReadings(me.STATISTICS_DATA_URL, readings);
+      me.doLoadReadings(me.STATISTICS_DATA_URL, readings, null, null, function(series, period) {
+        $(document).trigger('TempChart_readings_loaded', [series, period]);
+        console.log('EVENT FIRE READINGS_LOADED ::', series, period);
+      });
     });
 
     $(document).bind('TempChart_load_sensors', function(e) { 
@@ -102,34 +105,39 @@ TempChart.DataHandler.prototype = {
       this.doLoadReadings(this.DATA_URL, period, from, to);
     } else {
       for (var i = 0; i < this.DATA_URL.length; i++) {
-        this.doLoadReadings(this.DATA_URL[i], period, from, to);
+        this.doLoadReadings(this.DATA_URL[i], period, from, to, function(series, period) {
+          this.nrOfLoadedDataSources++;
+          if(typeof this.DATA_URL !== 'string' && this.nrOfLoadedDataSources === this.DATA_URL.length) {
+            $(document).trigger('TempChart_readings_loaded', [series, period]);
+            console.log('EVENT FIRE READINGS_LOADED ::', series, period);
+          }
+        }.bind(this));
       };
     }
   },
 
-  doLoadReadings: function(data_url, period, from, to, last_load) {
+  doLoadReadings: function(data_url, period, from, to, callback) {
     $(document).trigger('TempChart_in_progress', [true]);
     var me = this;    
     var params = {
-        tempstring: (new Date()).getTime(),
+      tempstring: (new Date()).getTime(),
     };
 
     if(typeof me.USE_CACHE !== 'undefined') { params.usecache = me.USE_CACHE; }
     if(typeof period !== 'undefined' && period !== null) { params.period = period; }
-    if(from) {   params.from = from; }
+    if(from) { params.from = from; }
     if(to) {   params.to = to; } 
     me.doRequest({
       url: data_url + '?' + $.param(params)
     },function(dataArray) {
-      me.nrOfLoadedDataSources++;
-       $.each(dataArray, function(j, data) {
-                var seriesData;
-                
-                if (data.length > 3) {
-                    seriesData = data[4];
-                }
-                
-                if (data[3] == 0 || data[3] == 'f0') { // temp (and forecast temp)
+      $.each(dataArray, function(j, data) {
+        var seriesData;
+        
+        if (data.length > 3) {
+          seriesData = data[4];
+        }
+          
+        if (data[3] == 0 || data[3] == 'f0') { // temp (and forecast temp)
           me.series.push({
                     name: data[1],
                     color: data[2],
@@ -165,15 +173,13 @@ TempChart.DataHandler.prototype = {
           });
         }
         else {
-           me.series.push({
+          me.series.push({
                     data: data
                   });
         }
-        
-        });
-            if(typeof me.DATA_URL !== 'string' && me.nrOfLoadedDataSources === me.DATA_URL.length) {
-        $(document).trigger('TempChart_readings_loaded', [me.series, period]);
-        console.log('EVENT FIRE READINGS_LOADED ::', me.series, period);
+      });
+      if(callback) {
+        callback(me.series, period);
       }
     });
   },
