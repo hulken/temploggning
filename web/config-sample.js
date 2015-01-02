48bd27771aaf62c0a98179c01c3bb38ee1928378
semhul <martin.hultman@sweco.se>
2015-01-02 19:24:42 +0100

    Corrected configuration

--- web/config-sample.js
+++ web/config-sample.js
@@ -1,7 +1,7 @@
 var CLIENT_CONFIG = {
     MAIN_ELEMENT_ID: 'graph',
     CONTROLS_ELEMNT_ID: 'controls',
-    STATISTICS_DATA_URL: '/temploggning/drakkedata/customreadings',
+    STATISTICS_DATA_URL: 'data/customreadings',
     DATA_URL: [
     	'data/readings',
         'data/weatherforecast/all?lat=58.8471&lng=16.9295&place=Sweden/Södermanland/Råby-Rönö'
